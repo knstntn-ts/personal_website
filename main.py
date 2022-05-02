4e3ca78c32fafa666035f1ccee9e58b4218d8f30
knstntn-ts <79147005+knstntn-ts@users.noreply.github.com>
2022-05-02 08:27:33 +0300

    Re-design of the whole page. Customization for my own needs.

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/current_work')
+def current_work():
+    return render_template('current_work.html')
 
 
 if __name__ == '__main__':
